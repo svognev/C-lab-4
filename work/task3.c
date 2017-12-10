ee7f1064ac181b7dec62aed5904e3fa41ab412b0
efelzz <vatimofe@gmail.com>
2017-12-10 11:46:02 +0300

    Tasks 3 of 6 done

---
+++ work/task3.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "task3.h"
+#define N 25
+
+int isPalindrome(char * str) {
+	
+	char *start = str;
+	char *end = str + (strlen(str) - 1);
+	
+	while (start < end)
+	{
+		if (*start == *end) {
+			start++;
+			end--;
+		}
+		else
+			return 0;
+	}
+	return 1;
+}
