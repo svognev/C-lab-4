a32cdc2ec08b5b0f2b96b31a30cd6694d3d85447
efelzz <vatimofe@gmail.com>
2017-12-20 16:34:24 +0300

    added '#include <string.h>'

--- work/task1.c
+++ work/task1.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "task1.h"
 #include "task4.h"
 #include <time.h>
--- work/task3.c
+++ work/task3.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 #include "task3.h"
 #define N 25
 
