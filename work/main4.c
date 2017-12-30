8a33012ccf0b43bc3966b65bbfc4fe33e9ecdcb1
Ksenia <imac@iMac-iMac.Dlink>
2017-12-30 17:04:10 +0300

    add main4 task4.c task4.h in.txt out.txt

---
+++ work/main4.c
@@ -0,0 +1,41 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "task4.h"
+
+#define MAXLINE 4096
+#define IFNAME "in.txt"
+#define OFNAME "out.txt"
+
+
+
+
+int main() {
+    FILE *ifp, *ofp;
+    char *str[MAXLINE];
+    char s[MAXLINE];
+    
+    int i, size;
+    
+    ifp=fopen(IFNAME,"r");
+    
+   if(ifp==NULL || (ofp=fopen(OFNAME, "w"))==NULL)
+   {
+       perror("fopen");
+       return -1;
+   }
+    for(i=0;i<MAXLINE;)
+    {
+        if(fgets(s, sizeof(s), ifp)==NULL)
+            break;
+        if(*s !='\n')
+            str[i++] = strdup(s);
+    }
+    size=i;
+    lineSort(str, size);
+    printLinesToFile((const char**)str, size, ofp);
+    
+    fclose(ifp);
+    fclose(ofp);
+    return 0;
+}
