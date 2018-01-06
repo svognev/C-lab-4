abc65f5f3651ac1657b806a46ad9b16135b34154
Andrey A Molkov <molkov_aa@mail.ru>
2018-01-06 17:52:08 +0300

    fix int for compare

--- work/task1.c
+++ work/task1.c
@@ -1,9 +1,6 @@
 #include "task1.h"
-#include<string.h>
-#include<stdlib.h>
-#include<stdio.h>
-#define SIZE 220
-void compare(const void *a,const void *b)
+
+int compare(const void *a,const void *b)
 {
 	if (strlen(*(char **)a) < strlen(*(char **)b))
 	{
@@ -12,11 +9,11 @@ void compare(const void *a,const void *b)
 	else
 		return -1;
 }
-void lineSort(char *str[], int size)
+void lineSort(char *str[SIZE], int size)
 {
-	qsort(str, size,sizeof(char*), compare);//base functi0n quick sort
+	qsort(str, size,sizeof(char **), compare);//base functi0n quick sort
 }
-void printLines(const char *str[], int size)
+void printLines(const char *str[SIZE], int size)
 {
 	int i = 0;
 	while (i<size)
