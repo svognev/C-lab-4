2727a6a02488e873438d9704d26c7c7affab8b2d
Ksenia <imac@iMac-iMac.Dlink>
2018-01-23 00:06:05 +0300

    fixed task3.c

--- work/main3.c
+++ work/main3.c
@@ -6,22 +6,22 @@
 #define N 20
 
 int main() {
-   
+    
     char str[N]={0};
     printf("Enter a string: \n");
     fgets(str, sizeof(str), stdin);
-
-	str[strlen(str) - 1] = '\0';
-
-	if (isPalindrome(str))
-	{
-		printf("%s is palindrome\n", str);
-	}
-	else
-	{
-		printf("%s is not a palindrome\n", str);
-	}
-
-	return 0;
-   
+    
+    str[strlen(str) - 1] = '\0';
+    
+    if (isPalindrome(str))
+    {
+        printf("%s is palindrome\n", str);
+    }
+    else
+    {
+        printf("%s is not a palindrome\n", str);
+    }
+    
+    return 0;
+    
 }
--- work/task3.c
+++ work/task3.c
@@ -4,16 +4,16 @@
 
 int isPalindrome(char *str)
 {
-    int lenght;
+   // int lenght;
     int flag=1;
-    lenght=strlen(str);
-    char *first=str;
-    char *last=str[lenght-1];
+    size_t lenght=strlen(str);
+   // char *first=str;
+   // char *last=str[lenght-1];
     
     for(int i=0;i<(lenght-1)/2;i++)
     {
-      
-	if(str[i] != str[lenght-1-i])
+        
+        if(str[i] != str[lenght-1-i])
         {
             flag=0;
             break;
@@ -21,6 +21,6 @@ int isPalindrome(char *str)
     }
     
     
-  return flag;
-
+    return flag;
+    
 }
