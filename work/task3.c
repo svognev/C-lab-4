b8a5feefbd87b790b0b74144e68c394985afe0d0
MartianovAV <andrei.martianoff92@yandex.ru>
2018-02-04 02:07:44 +0300

    correct task3.h

--- work/task3.c
+++ work/task3.c
@@ -1,3 +1,4 @@
+
 int isPalindrome(char * str)	//функция, проверяющая str (ответ либо 0, либо 1)
 {
 	int len = strlen(str);
@@ -7,12 +8,38 @@ int isPalindrome(char * str)	//функция, проверяющая str (ответ либо 0, либо 1)
 	char *praP;	//Указатель на последний элемент строки
 	levP = &(str[0]);
 	praP = &(str[len - 1]);
-	for(; (levP != praP) && (levP != (praP - 1)); levP++, praP--)
+	while ((levP != praP) && (levP != (praP - 1)))
 	{
-		printf("*levP = %c, *praP = %c\n", *levP, *praP);
+		//Переводим все буквы в нижний регистр
+		for (int i = 0; i <= len; i++)
+		{
+			if (str[i] > 64 && str[i] < 91)
+			{
+				str[i] = str[i] + 32;
+			}
+		}
+		//Пропускаем пробелы и другие знаки препинания
+		if (*levP == ' ' || *levP == ',' || *levP == '.' || *levP == "'" || *levP == "?" || *levP == "!")
+		{
+			levP++;
+			continue;
+		}
+		//Пропускаем пробелы
+		if (*praP == ' ' || *praP == ',' || *praP == '.' || *praP == "'" || *praP == "?" || *praP == "!")
+		{
+			praP--;
+			continue;
+		}
+		//printf("*levP = %c, *praP = %c\n", *levP, *praP);
+		//Проверяем символы на равенство друг другу, если не равны возращаем 0
 		if (*levP != *praP)
 		{
 			return 0;
+		}
+		else
+		{
+			levP++;
+			praP--;
 		}
 	}
 	return 1;
