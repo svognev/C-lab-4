40b52e6b8a59e87aa5dcb05267c091ef4aa41b62
MartianovAV <andrei.martianoff92@yandex.ru>
2018-02-10 22:50:05 +0300

    corr task3.c

--- work/task3.c
+++ work/task3.c
@@ -5,10 +5,10 @@
 int isPalindrome(char * str)	//функция, проверяющая str (ответ либо 0, либо 1)
 {
 	int len = strlen(str);
-	if(str[len - 1] == '\n')
+	/*if(str[len - 1] == '\n')
 	{
 		str[len - 1] = '\0';	//Заменяем '\n' на '\0'
-	}
+	}*/
 	len = strlen(str);
 	char *levP;	//Указатель на первый элемент строки
 	char *praP;	//Указатель на последний элемент строки
@@ -17,35 +17,37 @@ int isPalindrome(char * str)	//функция, проверяющая str (ответ либо 0, либо 1)
 	while ((levP != praP) && (levP != (praP - 1)))
 	{
 		//Переводим все буквы в нижний регистр
-		for (int i = 0; i <= len; i++)
+		/*for (int i = 0; i <= len; i++)
 		{
 			if (str[i] >= 'A' && str[i] <= 'Z')
 			{
 				str[i] = str[i] + 32;
 			}
 		}
+		*/
+
 		//Пропускаем пробелы и другие знаки препинания
-		if ((*levP) == ' ' || (*levP) == ',' || (*levP) == '.' || (*levP) == '\'' || (*levP) == '?' || (*levP) == '!')
+		if ((*levP) == ' ' || (*levP) == ',' || (*levP) == '.' || (*levP) == '\'' || (*levP) == '?' || (*levP) == '!' || (*levP) == '\n' || (*levP) == '-' || (*levP) == '\"')
 		{
 			levP++;
 			continue;
 		}
 		//Пропускаем пробелы
-		if ((*praP) == ' ' || (*praP) == ',' || (*praP) == '.' || (*praP) == '\'' || (*praP) == '?' || (*praP) == '!')
+		if ((*praP) == ' ' || (*praP) == ',' || (*praP) == '.' || (*praP) == '\'' || (*praP) == '?' || (*praP) == '!' || (*praP) == '\n' || (*praP) == '-' || (*praP) == '\"')
 		{
 			praP--;
 			continue;
 		}
 		//printf("*levP = %c, *praP = %c\n", *levP, *praP);
 		//Проверяем символы на равенство друг другу, если не равны возращаем 0
-		if (*levP != *praP)
-		{
-			return 0;
-		}
-		else
+		if ((*levP == (*praP + 32)) || (*praP == (*levP + 32)) || (*levP == *praP))
 		{
 			levP++;
 			praP--;
+		}
+		else
+		{
+			return 0;
 		}
 	}
 	return 1;
